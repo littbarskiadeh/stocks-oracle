82076f6774f6131ae596e44cd78007205f612ab0
littbarski <lit.adeh@gmail.com>
2021-02-05 10:22:21 -0500

    fixed hex values for setStock

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import Web3 from 'web3'
-
-
 import {CONTRACT_ADDRESS, ABI, URL} from './config.js'
 
 class App extends Component {
@@ -16,14 +14,25 @@ class App extends Component {
 
         const accounts = await web3.eth.getAccounts()
         this.setState({ accounts: accounts })
-        // console.log(accounts)
 
-        const stocks = new web3.eth.Contract(ABI,CONTRACT_ADDRESS)
-        this.setState({ stocks })
-        // stocks.methods.setStock(["0x41","0x42","0x43","0x44"], 100, 2000).call()
+        const contractInstance = new web3.eth.Contract(ABI,CONTRACT_ADDRESS)
+        
+        // Set the stock values
+        const symbol = web3.utils.utf8ToHex("ABC");
+        const price = 100;
+        const volume = 200000;
+        contractInstance.methods.setStock(symbol, price, volume)
+            .send({from: accounts[0]}).on('receipt', () => {
+                console.log('set stock done!');
+            })
+        
+        const stockPrice = await contractInstance.methods.getStockPrice(symbol).call();
+        console.log('stock price', stockPrice);
+        this.setState({stockPrice})
+
         // const symbol = ["0x41","0x42","0x43","0x44"];
-        await stocks.methods.setMessage(80).call();
-        const stocksMessage = await stocks.methods.getMessage().call();
+        await contractInstance.methods.setMessage(80).call();
+        const stocksMessage = await contractInstance.methods.getMessage().call();
         this.setState({ message: stocksMessage })
 
         console.log('CONTRACT: ----->>>>> ', stocksMessage)
@@ -50,9 +59,9 @@ class App extends Component {
         return (
             <div className="value">
                 <button onClick={this.clickHandler}> Click me! </button>
-
-                <h2>Contract message: {this.state.message}</h2><br/>
+                <h4>Stock Price: {this.state.stockPrice}</h4><br/>
                 
+                <h2>Accounts</h2><br/>
                 <ul>
                     {this.state.accounts.map(account => <li key={account}>{account}</li> )}
                 </ul>
